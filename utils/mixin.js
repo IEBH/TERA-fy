713e26b9184b3633e887b4b59e8d56e629617b22
Matt Carter <m@ttcarter.com>
2024-02-21 15:18:21 +1000

    refactor: Move horrible mixin() handler into its own utility library

---
+++ utils/mixin.js
@@ -0,0 +1,18 @@
+/**
+* Shallow-copy a object instance and inject new properties into the result
+*
+* Rather ugly shallow-copy-of instance hack from https://stackoverflow.com/a/44782052/1295040
+* Keeps the original object instance and overrides the given object of assignments
+*
+* @param {Object} instance Original object class instance to mixin
+* @param {Object} assignments Additional object properties to mix
+* @returns {Object} A shallow copy of the input instance extended with the assignments
+*/
+export default function mixin(instance, assignments) {
+	let output = Object.assign(
+		Object.create(Object.getPrototypeOf(instance)),
+		instance,
+		assignments,
+	);
+	return output;
+}
